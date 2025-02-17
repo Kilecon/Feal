fd58ccfe5f313ebbd6834a1c81499aed7957220b
Mathieu Barboteau <mathieu.barboteau@ynov.com>
2025-02-17 15:06:48 +0100

    [FEAT] Onboarding, firsts steps

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
-import { Link, Stack } from 'expo-router';
+import { Stack, Link, Redirect } from 'expo-router';
 
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import { HorizontalScrollView } from "~/components/HorizontalSlider"
 
 export default function Home() {
+  const isOnboarded = false;
+  if (!isOnboarded) {
+    return <Redirect href="/onboarding" />;
+  }
   return (
     <>
       <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
+        <HorizontalScrollView/>
       </Container>
     </>
   );
---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router'
+import { OnboardingScreen } from '~/screens/OnboardingScreen'
+
+export default function Onboarding() {
+    return (
+    <>
+    <Stack.Screen options={{ gestureEnabled: false}}/>
+    <OnboardingScreen/>
+    </>
+    );
+}

9edaf32395e74b292d447360c3047faa958aad49
LeoMa33 <leo.mace@ynov.com>
2025-02-18 17:15:29 +0100

    [FEAT] start circular progress

--- components/InformationCard.tsx
+++ components/InformationCard.tsx
@@ -2,6 +2,8 @@ import {Box, Text, theme, Theme} from 'theme';
 import {Chips} from "~/components/Chips";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import Svg, {Circle, Text as SvgText} from "react-native-svg";
+import {useEffect, useState} from "react";
 
 type InformationCardProps = {
     label: string;
@@ -10,6 +12,17 @@ type InformationCardProps = {
 };
 
 export const InformationCard = ({label, color = 'green', icon}: InformationCardProps) => {
+
+    const [circumference, setCircumference] = useState(0);
+
+    const radius = 20;
+
+    useEffect(() => {
+        const circumferenceValue = 2 * Math.PI * radius;
+        setCircumference(circumferenceValue);
+    }, [radius]);
+    const strokeDashoffset = circumference * (1 - 40);
+
     return (
         <Box
             borderRadius="base"
@@ -19,6 +32,29 @@ export const InformationCard = ({label, color = 'green', icon}: InformationCardP
             alignItems="center"
             gap="sm_12"
         >
+            <Svg width={radius * 2} height={radius * 2}>
+                <Circle
+                    stroke={color}
+                    fill="transparent"
+                    strokeWidth={2}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    cx={radius}
+                    cy={radius}
+                    r={radius - 2 / 2}
+                />
+                <SvgText
+                    x="50%"
+                    y="50%"
+                    textAnchor="middle"
+                    alignmentBaseline="middle"
+                    fontSize={radius / 2.5}
+                    fill={color}
+                    fontWeight="bold"
+                >
+                    10
+                </SvgText>
+            </Svg>
             <FontAwesomeIcon icon={icon} color={theme.colors[color]} size={16}/>
             <Box
                 gap="s_8"
